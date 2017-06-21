228ed23d543dbe5896ae7e9cf1b47a8618ac37a4
tamilselvim <tamilselvi.violet1@gmail.com>
2017-06-21 14:12:04 +0530

    Add files via upload

---
+++ classs.java
@@ -0,0 +1,15 @@
+package maxminno;
+
+public class classs {
+
+	public static void main(String[] args) {
+		int i,fact=1;
+		int num=5;
+		for(i=1;i<=num;i++){
+			fact=fact*i;
+		}
+		System.out.println("factorial of number"+fact);
+
+	}
+
+}
