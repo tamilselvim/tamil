dd133429bf5b31c02d748148e5f7e92f3925bc6c
tamilselvim <tamilselvi.violet1@gmail.com>
2017-07-03 17:29:47 +0530

    Add files via upload

---
+++ fact.java
@@ -0,0 +1,16 @@
+package guvi;
+import java.util.*;
+public class fact {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,fact=1;
+		int n=5;
+		for(i=1;i<=n;i++){
+			fact=fact*i;
+		}
+		System.out.println("factorial of a+ number+"+fact);
+
+	}
+
+}
