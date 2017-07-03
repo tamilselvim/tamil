53a2ff3e7af9443159ee697a0be8cb88cf027a9a
tamilselvim <tamilselvi.violet1@gmail.com>
2017-07-03 17:38:38 +0530

    Add files via upload

---
+++ even.java
@@ -0,0 +1,17 @@
+package guvi;
+import java.util.*;
+public class even {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n=12;
+		if(n%2==0){
+			System.out.println("even");
+			
+		}
+		else
+			System.out.println("odd");
+
+	}
+
+}
