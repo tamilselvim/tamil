89b4f7de906638e5b2316770223e0d8d2f47ec28
tamilselvim <tamilselvi.violet1@gmail.com>
2017-07-03 17:11:15 +0530

    Add files via upload

---
+++ oddeven.java
@@ -0,0 +1,19 @@
+package lc1;
+import java.io.*;
+import java.util.*;
+public class oddeven {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		System.out.println("enter");
+		int a=s.nextInt();
+		if(a%2==0){
+			System.out.println("even");
+		}
+		else
+			System.out.println("odd");
+
+	}
+
+}
