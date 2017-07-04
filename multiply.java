1a6cc2bb9e6ebd2d192220703247fe4f6ebff89f
tamilselvim <tamilselvi.violet1@gmail.com>
2017-07-04 13:55:06 +0530

    Add files via upload

---
+++ multiply.java
@@ -0,0 +1,16 @@
+package guvi;
+import java.util.*;
+public class multiply {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n,i;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		for(i=1;i<=20;i++){
+			System.out.println(n+"*"+i+"="+n*i);
+		}
+
+	}
+
+}
