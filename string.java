f1959ccbf7258ee70b4a182dc829fa6bed119b7d
tamilselvim <tamilselvi.violet1@gmail.com>
2017-07-04 11:36:00 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,14 @@
+package guvi;
+import java.util.*;
+public class string {
+
+	public static String main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]={11};
+		String str=String.valueOf(a).substring(0,1);
+		System.out.println(str);
+		return str+str+str;
+
+	}
+
+}
