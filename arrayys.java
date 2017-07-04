4d22b19503763fd30b7896a63da6da090906dcee
tamilselvim <tamilselvi.violet1@gmail.com>
2017-07-04 14:21:34 +0530

    Add files via upload

--- arrayys.java
+++ arrayys.java
@@ -4,9 +4,11 @@ public class arrayys {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a[]={11};
+		int a=11;
 		String str=String.valueOf(a).substring(0,1);
+		
 		System.out.println(str);
+	
 		
 
 	}
