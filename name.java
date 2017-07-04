f4a8ab6e0a1d592a5a55a8e72882645f92e168c6
tamilselvim <tamilselvi.violet1@gmail.com>
2017-07-04 15:34:58 +0530

    Add files via upload

---
+++ name.java
@@ -0,0 +1,27 @@
+package guvi;
+import java.util.*;
+public class name {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i,j,sum=0;
+		for(i=1;i<=15;i++){
+			for(j=1;j<=i;j++){
+				sum=sum+j;}
+				System.out.println(sum);
+				
+				sum=0;
+			
+		}
+		for(int q=15;q<=45;q++){
+			
+			if(q%2!=0){
+			sum=q+sum;}
+			System.out.println("sum of odd number in 15to45:"+sum);
+				
+				}
+			}
+
+	}
+
+
