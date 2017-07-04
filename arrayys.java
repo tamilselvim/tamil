08a18b542dff27d898774311f223c18b146c4719
tamilselvim <tamilselvi.violet1@gmail.com>
2017-07-04 11:39:25 +0530

    Add files via upload

---
+++ arrayys.java
@@ -0,0 +1,14 @@
+package guvi;
+import java.util.*;
+public class arrayys {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a[]={11};
+		String str=String.valueOf(a).substring(0,1);
+		System.out.println(str);
+		
+
+	}
+
+}
