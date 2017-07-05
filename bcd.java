1e2047bb9b47c611263a077c080993a909717ce9
tamilselvim <tamilselvi.violet1@gmail.com>
2017-07-05 16:31:19 +0530

    Add files via upload

--- bcd.java
+++ bcd.java
@@ -1,9 +1,20 @@
 package aaa;
-
+import java.util.*;
 public class bcd {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int n;
+		int sum=0;
+		n=s.nextInt();
+		for(int i=1;i<=n;i++){
+			int r=n%10;
+			sum=sum*10+r;
+			n=n/10;
+		}
+		System.out.println(sum);
+
 
 	}
 
