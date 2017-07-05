dcf86ff8bc54f631552b3ea4c2fe957543007a11
tamilselvim <tamilselvi.violet1@gmail.com>
2017-07-05 17:08:07 +0530

    Add files via upload

---
+++ number.java
@@ -0,0 +1,21 @@
+package aaa;
+import java.util.*;
+public class number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int n;
+		Scanner s=new Scanner(System.in);
+		n=s.nextInt();
+		int count=0;
+		for(int i=1;i<=15;i++){
+			if(n%2!=0){
+				count++;}
+				System.out.println("count the odd number"+count);
+			}
+			
+		}
+
+	}
+
+
