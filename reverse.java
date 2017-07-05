83e4948f9f8901daa7b5e134a0bf3c828bdd3229
tamilselvim <tamilselvi.violet1@gmail.com>
2017-07-05 16:28:01 +0530

    Add files via upload

--- reverse.java
+++ reverse.java
@@ -1,26 +1,18 @@
-package String;
+package aaa;
 import java.util.*;
 public class reverse {
-
-	public static void main(String[] args) {
+public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String str=" ";
 		Scanner s=new Scanner(System.in);
-		System.out.println("enter the string");
-		str=s.nextLine();
-		StringBuffer s1=new StringBuffer(str);
-		StringBuffer s2=new StringBuffer(s1);
-		s1.reverse();
-		System.out.println("origional string"+str);
-		System.out.println("rreverse string"+s1);
-		if(String.valueOf(s1).compareTo (String.valueOf(s2))==0){
-			System.out.println("s2 is palindrome"+s2);
-			
+		int n;
+		int sum=0;
+		n=s.nextInt();
+		for(int i=1;i<=n;i++){
+			int r=n%10;
+			sum=sum*10+r;
+			n=n/10;
 		}
-		else
-			System.out.println("s2 is not a palindrome"+s2);
-		
-		
+		System.out.println(sum);
 
 	}
 
