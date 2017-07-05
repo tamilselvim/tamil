b605467f4502a5b6257a1d3f7eb3d3b8421bc379
tamilselvim <tamilselvi.violet1@gmail.com>
2017-07-05 16:56:26 +0530

    Add files via upload

---
+++ ascii.java
@@ -0,0 +1,28 @@
+package aaa;
+import java.util.*;
+public class ascii {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char ch;
+		Scanner s=new Scanner(System.in);
+		ch=s.next().charAt(0);
+		if(ch>=65&&ch<=90){
+			System.out.println("ascii value");
+			
+		}
+		else{
+			System.out.println("is not ascii value");
+		}
+			
+		
+		}
+		
+		
+		
+		
+		
+
+	}
+
+
