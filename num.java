84d34393054bb23d1ed080b65c98dfdfa21ec438
tamilselvim <tamilselvi.violet1@gmail.com>
2017-07-06 17:12:17 +0530

    Add files via upload

---
+++ num.java
@@ -0,0 +1,13 @@
+package tamil;
+
+public class num {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String str="hello";
+		int a=str.length();
+		System.out.println(str.length());
+
+	}
+
+}
