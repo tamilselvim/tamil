6c3ca4b33c92d1cbfd39e4c7480380c820d26aef
tamilselvim <tamilselvi.violet1@gmail.com>
2017-07-06 17:08:38 +0530

    Add files via upload

---
+++ fibonacci.java
@@ -0,0 +1,20 @@
+package tamil;
+import java.util.*;
+public class fibonacci {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a1=0,a2=1,a3;
+		int count=10;
+		System.out.println(a1+""+a2);
+		for(int i=2;i<count;++i) {
+			a3=a1+a2;
+			
+		
+		System.out.println(""+a3);
+		
+a1=a2;
+a2=a3;}
+	}
+
+}
