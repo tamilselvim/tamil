625e3f788325a4c5a6378313b3ceb593108981b6
tamilselvim <tamilselvi.violet1@gmail.com>
2017-07-10 17:31:31 +0530

    Add files via upload

---
+++ abc.java
@@ -0,0 +1,19 @@
+package tamil;
+import java.util.*;
+public class abc {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		int c;
+		c=a;
+		a=b;
+		b=c;
+		System.out.println("swaping the number a:"+a);
+		System.out.println("swaping the number b:"+b);
+
+
+	}
+
+}
