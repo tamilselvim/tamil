462ecff9a828a2b22ecf66e7c6f2a3c7dfee7eff
tamilselvim <tamilselvi.violet1@gmail.com>
2017-07-11 16:40:12 +0530

    Add files via upload

---
+++ bitwise.java
@@ -0,0 +1,17 @@
+package guvi;
+
+public class bitwise {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     int a=10;
+     int b=20;
+   
+     a=a^b;
+     b=a^b;
+     a=a^b;
+     System.out.println("swaping the a is:"+a);
+     System.out.println("swaping the b is:"+b);
+	}
+
+}
